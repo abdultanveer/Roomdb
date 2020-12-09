e3301058b042118010edbfd97074f6d5f9973451
abdul <abdul.tanveer@gmail.com>
2020-12-09 07:31:29 +0530

    sms broadcastreceiver

---
+++ app/src/main/java/com/next/roomdb/SmsReceiver.java
@@ -0,0 +1,16 @@
+package com.next.roomdb;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "abdul u 've received an sms", Toast.LENGTH_SHORT).show();
+    }
+}
