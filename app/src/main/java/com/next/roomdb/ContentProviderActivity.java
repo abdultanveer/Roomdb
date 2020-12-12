c625ea5431ad621fabdb75526180d67224c6926a
abdul <abdul.tanveer@gmail.com>
2020-12-12 11:43:41 +0530

    ContentProviderActivity -- querying in an loader

--- app/src/main/java/com/next/roomdb/ContentProviderActivity.java
+++ app/src/main/java/com/next/roomdb/ContentProviderActivity.java
@@ -1,6 +1,11 @@
 package com.next.roomdb;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
 import android.database.Cursor;
 import android.net.Uri;
@@ -9,29 +14,50 @@ import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class ContentProviderActivity extends AppCompatActivity {
+public class ContentProviderActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 ListView contentListView;
+    CursorAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_provider);
         contentListView = findViewById(R.id.contentproviderlist);
+        LoaderManager loaderManager = getSupportLoaderManager();//coolie manager
+        loaderManager.initLoader(123,null,this); //hiring a coolie
 
 
 
-        Uri uriSms = Uri.parse("content://sms/inbox");
 
-        DbQueryTask dbQueryTask = new DbQueryTask(contentListView,this);
-        dbQueryTask.execute(uriSms);
-       /* Cursor dataCursor = getContentResolver().query(uriSms, null,null,null,null);
+        /*DbQueryTask dbQueryTask = new DbQueryTask(contentListView,this);
+        dbQueryTask.execute(uriSms);*/
+       /* Cursor dataCursor = getContentResolver().query(uriSms, null,null,null,null);*/
 
-        CursorAdapter adapter = new SimpleCursorAdapter(this,
+         adapter = new SimpleCursorAdapter(this,
                 android.R.layout.simple_list_item_2,                    //layout of each row
-                dataCursor,                                             //data
+                null,                                             //data
                 new String[]{"body","address"},                         //column names
                 new int[]{android.R.id.text1,android.R.id.text2},       //textview ids
                 0);
 
-        contentListView.setAdapter(adapter);*/
+        contentListView.setAdapter(adapter);
+    }
+
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int coolieId, @Nullable Bundle instructionsLoaderManager) {//onboarding  -- from which warehourse/coach fetch the data/goods
+
+        Uri uriSms = Uri.parse("content://sms/inbox"); //location of warehourse/ bogie
+        return new CursorLoader(this,uriSms,null,null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) { //for salary
+            adapter.swapCursor(data); //swap out the null cursor with loaded cursor
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
